c75176fa19f64da4ebb8d870c9606067a8d19b93
Christian Nicola <hi@chrisnicola.de>
2021-11-19 14:28:52 +0100

    Commit initial dmenu build
    This build was patched with https://github.com/bakkeby/dmenu-flexipatch
    and then configured to suit my needs.

---
+++ config.def.h
@@ -0,0 +1,41 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom */
+static int opacity = 1;                     /* -o  option; if 0, then alpha is disabled */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] =
+{
+	"BlexMono Nerd Font Mono:size=10:antialias=true:autohint=true",
+	"JoyPixels:size=8:antialias=true:autohint=true"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const unsigned int baralpha = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+};
+
+static
+const
+char *colors[][2] = {
+	/*               fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel]  = { "#eeeeee", "#005577" },
+	[SchemeOut]  = { "#000000", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+/* -g option; if nonzero, dmenu uses a grid comprised of columns and lines */
+static unsigned int columns    = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+
---
+++ patch/include.c
@@ -0,0 +1,2 @@
+#include "mousesupport.c"
+#include "numbers.c"
